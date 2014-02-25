214e61944b328b5168d8cf11956264d74fa6a670
Nick Jennings <nick@silverbucket.net>
2014-02-25 10:07:02 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 grunt &&
 cp -r articles/ build/ &&
 cp atom.xml build/ &&
-git add build/ &&
-git commit -m "deploy" . &&
-git push &&
+git add build/
+git commit -m "deploy" . 
+git push 
 git subtree push --prefix build/ 5apps master
